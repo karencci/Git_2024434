279de3f69e91c638df431b36e6b2475fad0dfc31
karencci <2024434@student.cct.ie>
2024-11-27 14:39:37 +0000

    Homework project Git

---
+++ src/git_2024434/Git_2024434.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024434;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Git_2024434 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Ken!");
+  
+        
+        
+       
+   
+   
+    
+    }
+    
+}
